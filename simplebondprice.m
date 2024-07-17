1b4d312565937d3ebe6d1d452f90ada1d98c95e8
William Guo <william.guo@us.kbr.com>
2024-07-17 12:51:20 -0700

    new commit

---
+++ simplebondprice.m
@@ -0,0 +1,8 @@
+function price = simplebondprice(face_value, coupon_payment, interest_rate, num_payments)
+    M = face_value;
+    C = coupon_payment;
+    N = num_payments;
+    i = interest_rate;
+
+    price = C * ( (1 - (1 + i)^-N) / i) + M * (1 + i)^-N;
+end
